be9a20c926f01eeb34036e51b53f3e6940f7d215
Salami-Taiwo-Oyekunle <salamitaiwo@yahoo.com>
2024-12-26 18:12:13 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+n1=12
+n2=3
+print('addition:',n1+n2)
+print('subtraction:', n1-n2)
+print('multiplication:', n1*n2)
+print('division:', n1/n2)
+print('floor division:', n1//n2)
+print('remainder:', n1%n2)
+print('exponential:', n1**n2)
